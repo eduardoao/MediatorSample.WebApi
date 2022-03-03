033c08c5dc0e53f87c7b11ca4666c5c3228dfd94
Eduardo Alcantara de Oliveira <eoliveira@trt4.jus.br>
2022-03-03 17:22:06 -0300

    exception finished

---
+++ MediatorSample.WebApi/SomeException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace MediatorSample.WebApi
+{
+    public class SomeException : Exception
+    {
+        public SomeException() : base()
+        {
+        }
+        public SomeException(string message) : base(message)
+        {
+        }
+        public SomeException(string message, params object[] args) : base(String.Format(CultureInfo.CurrentCulture, message, args))
+        {
+        }
+    }
+}
